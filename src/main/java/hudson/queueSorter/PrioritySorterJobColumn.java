1a820393bff51275b3b6dffa986b84e8fedd7454
cjo9900 <cjo.johnson@gmail.com>
2011-01-09 17:24:46 +0000

    Update licence in java file and correct line endings

--- src/main/java/hudson/queueSorter/PrioritySorterJobColumn.java
+++ src/main/java/hudson/queueSorter/PrioritySorterJobColumn.java
@@ -1,3 +1,24 @@
+/*
+ * The MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package hudson.queueSorter;
 
 import hudson.Extension;
@@ -15,36 +36,34 @@ import org.kohsuke.stapler.StaplerRequest;
  * 
  * The column displays priority set for the job.
  *
- *
- *
- * @author c3johnso
  */
-public class PrioritySorterJobColumn extends ListViewColumn{
+public class PrioritySorterJobColumn extends ListViewColumn {
 
-    public String getPriority(Job job)
-    {
+    public String getPriority(Job job) {
         PrioritySorterJobProperty jp = (PrioritySorterJobProperty) job.getProperty(PrioritySorterJobProperty.class);
         if (jp != null) {
             return Integer.toString(jp.priority);
         } else {
-            // No priority has been set for this job - use empty
+            // No priority has been set for this job - use empty as defined in PrioritySorterQueueSorter
+            // This should be defined so that it is common and can be accesed as an int or string for the two different classes.
             return "100";
         }
     }
-
     @Extension
     public static final Descriptor<ListViewColumn> DESCRIPTOR = new DescriptorImpl();
-    
+
     @Override
     public Descriptor<ListViewColumn> getDescriptor() {
         return DESCRIPTOR;
     }
 
     private static class DescriptorImpl extends Descriptor<ListViewColumn> {
+
         @Override
         public ListViewColumn newInstance(StaplerRequest req, JSONObject formData) throws FormException {
             return new PrioritySorterJobColumn();
         }
+
         @Override
         public String getDisplayName() {
             return "Priority Value";
