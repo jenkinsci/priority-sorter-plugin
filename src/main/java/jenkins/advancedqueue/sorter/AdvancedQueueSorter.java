45a36e1799b55b8173d42d793c5242e3989b45c9
emsa23 <emsa@switchbeat.com>
2013-10-31 20:50:27 +0100

    Storing queue to disk to survive restarts.

--- src/main/java/jenkins/advancedqueue/sorter/AdvancedQueueSorter.java
+++ src/main/java/jenkins/advancedqueue/sorter/AdvancedQueueSorter.java
@@ -24,6 +24,8 @@
 package jenkins.advancedqueue.sorter;
 
 import hudson.Extension;
+import hudson.model.Describable;
+import hudson.model.Descriptor;
 import hudson.model.Queue.BuildableItem;
 import hudson.model.Queue.LeftItem;
 import hudson.model.Queue.WaitingItem;
@@ -43,58 +45,80 @@ import jenkins.advancedqueue.PrioritySorterConfiguration;
  * @since 2.0
  */
 @Extension
-public class AdvancedQueueSorter extends QueueSorter {
+public class AdvancedQueueSorter extends QueueSorter implements
+		Describable<AdvancedQueueSorter> {
 
-	// Keeps track of what weighted-prio each buildableItems item has
-	static Map<Integer, Float> item2weight = new HashMap<Integer, Float>();
+	static class AdvancedQueueSorterDescriptor extends
+			Descriptor<AdvancedQueueSorter> {
+
+		// Keeps track of what weighted-prio each buildableItems item has
+		Map<Integer, Float> item2weight = new HashMap<Integer, Float>();
+
+		@Override
+		public String getDisplayName() {
+			return "AdvancedQueueSorterDescriptor";
+		}
+
+	}
+
+	private final AdvancedQueueSorterDescriptor descriptor = new AdvancedQueueSorterDescriptor();
+
+	public Descriptor<AdvancedQueueSorter> getDescriptor() {
+		return descriptor;
+	}
+
+	public AdvancedQueueSorter() {
+		super();
+		descriptor.load();
+	}
 
 	@Override
 	public void sortBuildableItems(List<BuildableItem> items) {
 		// Handle Legacy mode
-		if(PrioritySorterConfiguration.get().getLegacyMode()) {
+		if (PrioritySorterConfiguration.get().getLegacyMode()) {
 			new PrioritySorterQueueSorter().sortBuildableItems(items);
-		} 
-		// Sort 
+		}
+		// Sort
 		Collections.sort(items, new Comparator<BuildableItem>() {
 			public int compare(BuildableItem o1, BuildableItem o2) {
-				float o1weight = item2weight.get(o1.id);
-				float o2weight = item2weight.get(o2.id);
-				if(o1weight > o2weight) {
+				float o1weight = descriptor.item2weight.get(o1.id);
+				float o2weight = descriptor.item2weight.get(o2.id);
+				if (o1weight > o2weight) {
 					return 1;
 				}
-				if(o1weight < o2weight) {
+				if (o1weight < o2weight) {
 					return -1;
 				}
 				return (int) (o1.getInQueueSince() - o2.getInQueueSince());
 			}
 		});
 	}
-	
+
 	public void onEnterWaiting(WaitingItem wi) {
 		final SorterStrategy prioritySorterStrategy = SorterStrategy
 				.getPrioritySorterStrategy(PrioritySorterConfiguration.get()
 						.getStrategy());
 		final float weight = prioritySorterStrategy.onNewItem(wi);
-		item2weight.put(wi.id, weight);
+		descriptor.item2weight.put(wi.id, weight);
+		descriptor.save();
 	}
 
 	public void onLeft(LeftItem li) {
 		final SorterStrategy prioritySorterStrategy = SorterStrategy
 				.getPrioritySorterStrategy(PrioritySorterConfiguration.get()
 						.getStrategy());
-		Float weight = item2weight.remove(li.id);
-		if(li.isCancelled()) {
+		Float weight = descriptor.item2weight.remove(li.id);
+		descriptor.save();
+		if (li.isCancelled()) {
 			prioritySorterStrategy.onCanceledItem(li);
 		} else {
 			prioritySorterStrategy.onStartedItem(li, weight);
 		}
-		
-	}
 
+	}
 
 	static public AdvancedQueueSorter get() {
-		return (AdvancedQueueSorter) QueueSorter.all().get(AdvancedQueueSorter.class);
+		return QueueSorter.all().get(AdvancedQueueSorter.class);
 	}
 
-	
 }
