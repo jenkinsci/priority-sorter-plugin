48296a8500222d8d0afbab7257de664f5e3fa553
emsa23 <emsa@switchbeat.com>
2013-11-10 17:12:22 +0100

    Made value transient to reset at restart.

--- src/main/java/jenkins/advancedqueue/sorter/strategy/FQBaseStrategy.java
+++ src/main/java/jenkins/advancedqueue/sorter/strategy/FQBaseStrategy.java
@@ -36,60 +36,60 @@ import jenkins.advancedqueue.PriorityConfiguration;
  * @since 2.0
  */
 abstract public class FQBaseStrategy extends MultiBucketStrategy {
-    //
-    static final protected float MIN_STEP_SIZE = 0.00001F;
-    // Keeps track on the last assigned weight for a given priority
-    static Map<Integer, Float> prio2weight = new HashMap<Integer, Float>();
-    // Keeps track on the max weight of started jobs
-    private float maxStartedWeight = 1F;
+	//
+	static final protected float MIN_STEP_SIZE = 0.00001F;
+	// Keeps track on the last assigned weight for a given priority
+	static Map<Integer, Float> prio2weight = new HashMap<Integer, Float>();
+	// Keeps track on the max weight of started jobs
+	transient private float maxStartedWeight = 1F;
 
-    public FQBaseStrategy() {
-    }
+	public FQBaseStrategy() {
+	}
 
-    public FQBaseStrategy(int numberOfPriorities, int defaultPriority) {
-        super(numberOfPriorities, defaultPriority);
-    }
-    
-    @Override
-    public void onStartedItem(LeftItem item, float weight) {
-        maxStartedWeight = Math.max(maxStartedWeight, weight);
-    }
+	public FQBaseStrategy(int numberOfPriorities, int defaultPriority) {
+		super(numberOfPriorities, defaultPriority);
+	}
 
-    public float onNewItem(Queue.Item item) {
-        int priority = PriorityConfiguration.get().getPriority(item);
-        float minimumWeightToAssign = getMinimumWeightToAssign(priority);
-        float weightToUse = getWeightToUse(priority, minimumWeightToAssign);
-        prio2weight.put(priority, weightToUse);
-        return weightToUse;
-    }
+	@Override
+	public void onStartedItem(LeftItem item, float weight) {
+		maxStartedWeight = Math.max(maxStartedWeight, weight);
+	}
 
-    protected float getMinimumWeightToAssign(int priority) {
-        Float minWeight = prio2weight.get(priority);
-        if (minWeight == null) {
-            return maxStartedWeight;
-        }
-        return Math.max(maxStartedWeight, minWeight);
-    }
+	public float onNewItem(Queue.Item item) {
+		int priority = PriorityConfiguration.get().getPriority(item);
+		float minimumWeightToAssign = getMinimumWeightToAssign(priority);
+		float weightToUse = getWeightToUse(priority, minimumWeightToAssign);
+		prio2weight.put(priority, weightToUse);
+		return weightToUse;
+	}
 
-    protected float getWeightToUse(int priority, float minimumWeightToAssign) {
-        float stepSize = getStepSize(priority);
-        double weight = Math.ceil(minimumWeightToAssign / stepSize) * stepSize;
-        // Cannot be smaller but maybe rounding problems (?)
-        if (weight <= minimumWeightToAssign) {
-            weight += stepSize;
-        }
-        // Protect us from values going through the roof if we run for a very
-        // long time
-        // This below might leave some jobs in the queue with very large weight
-        // this probably improbable to happen so let's do it like this for now
-        // ...
-        if (Double.POSITIVE_INFINITY == weight) {
-            maxStartedWeight = 1F;
-            prio2weight.clear();
-            return getWeightToUse(priority, minimumWeightToAssign);
-        }
-        return (float) weight;
-    }
+	protected float getMinimumWeightToAssign(int priority) {
+		Float minWeight = prio2weight.get(priority);
+		if (minWeight == null) {
+			return maxStartedWeight;
+		}
+		return Math.max(maxStartedWeight, minWeight);
+	}
 
-    abstract float getStepSize(int priority);
+	protected float getWeightToUse(int priority, float minimumWeightToAssign) {
+		float stepSize = getStepSize(priority);
+		double weight = Math.ceil(minimumWeightToAssign / stepSize) * stepSize;
+		// Cannot be smaller but maybe rounding problems (?)
+		if (weight <= minimumWeightToAssign) {
+			weight += stepSize;
+		}
+		// Protect us from values going through the roof if we run for a very
+		// long time
+		// This below might leave some jobs in the queue with very large weight
+		// this probably improbable to happen so let's do it like this for now
+		// ...
+		if (Double.POSITIVE_INFINITY == weight) {
+			maxStartedWeight = 1F;
+			prio2weight.clear();
+			return getWeightToUse(priority, minimumWeightToAssign);
+		}
+		return (float) weight;
+	}
+
+	abstract float getStepSize(int priority);
 }
