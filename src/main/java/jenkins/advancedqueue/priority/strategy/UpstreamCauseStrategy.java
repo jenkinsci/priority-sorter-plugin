fec56d8fd773ebffb33afbe1b07e3b6af108da81
emsa23 <emsa@switchbeat.com>
2013-12-08 12:44:11 +0100

    Added UpstreamCauseStrategy.
    This strategy will give the triggered Job the same priority
    as the Build it got triggered by.

---
+++ src/main/java/jenkins/advancedqueue/priority/strategy/UpstreamCauseStrategy.java
@@ -0,0 +1,80 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2013, Magnus Sandberg
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package jenkins.advancedqueue.priority.strategy;
+
+import hudson.Extension;
+import hudson.model.Cause;
+import hudson.model.Cause.UpstreamCause;
+import hudson.model.Queue;
+
+import java.util.List;
+
+import jenkins.advancedqueue.PrioritySorterConfiguration;
+import jenkins.advancedqueue.sorter.ItemInfo;
+import jenkins.advancedqueue.sorter.QueueItemCache;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * @author Magnus Sandberg
+ * @since 2.3
+ */
+public class UpstreamCauseStrategy extends AbstractDynamicPriorityStrategy {
+
+	@Extension
+	static public class BuildParameterStrategyDescriptor extends AbstractDynamicPriorityStrategyDescriptor {
+		public BuildParameterStrategyDescriptor() {
+			super("Job Triggered by a Upstream Build");
+		}
+	};
+
+	@DataBoundConstructor
+	public UpstreamCauseStrategy() {
+	}
+
+	private UpstreamCause getUpstreamCause(Queue.Item item) {
+		List<Cause> causes = item.getCauses();
+		for (Cause cause : causes) {
+			if (cause.getClass() == UpstreamCause.class) {
+				return (UpstreamCause) cause;
+			}
+		}
+		return null;
+	}
+
+	public int getPriority(Queue.Item item) {
+		int upstreamBuildId = getUpstreamCause(item).getUpstreamBuild();
+		ItemInfo upstreamItem = QueueItemCache.get().getItem(upstreamBuildId);
+		// Upstream Item being null should be very very rare
+		if (upstreamItem != null) {
+			return upstreamItem.getPriority();
+		}
+		return PrioritySorterConfiguration.get().getStrategy().getDefaultPriority();
+	}
+
+	@Override
+	public boolean isApplicable(Queue.Item item) {
+		return getUpstreamCause(item) != null;
+	}
+}
