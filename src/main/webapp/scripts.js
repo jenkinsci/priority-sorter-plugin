adb35b89aaa178f97887507366b04e80f10656ff
emsa23 <emsa@switchbeat.com>
2013-12-12 22:04:46 +0100

    [JENKINS-20995] Default Priority always shows 1-5
    Fixed problem where Default Priority Dropdown
    always showed 1-5.
    Updated default handling to reuse set values 
    when switching strategy.

--- src/main/webapp/scripts.js
+++ src/main/webapp/scripts.js
@@ -2,19 +2,21 @@ document.observe("dom:loaded", function() {
 	prioritySorterGlobalConfUpdate();
 });
 
+Element.prototype.triggerEvent = function(eventName)
+{
+    if (document.createEvent)
+    {
+        var evt = document.createEvent('HTMLEvents');
+        evt.initEvent(eventName, true, true);
+
+        return this.dispatchEvent(evt);
+    }
+
+    if (this.fireEvent)
+        return this.fireEvent('on' + eventName);
+}
+
 function prioritySorterGlobalConfUpdate()
 {
-	strategy=document.getElementById('ps_strategy').value;
-	allowPriorityOnJobs=document.getElementById('ps_allowPriorityOnJobs')
-	defaultPriority=document.getElementById('ps_defaultPriority')
-	numberOfPriorities=document.getElementById('ps_numberOfPriorities')
-	if(strategy == 'FIFO') {
-		allowPriorityOnJobs.disabled=true
-		numberOfPriorities.disabled=true
-		defaultPriority.disabled=true
-	} else  {
-		allowPriorityOnJobs.disabled=false
-		numberOfPriorities.disabled=false
-		defaultPriority.disabled=false
-	}
+	$('ps_numberOfPriorities').triggerEvent('change')
 }
