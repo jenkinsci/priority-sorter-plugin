df55f83d7f64f81fbca02bc9711f2144264942c5
emsa23 <emsa@switchbeat.com>
2013-12-15 23:37:54 +0100

    [TEST] Added tests for JobPattern

---
+++ src/test/java/jenkins/advancedqueue/test/JobPatternGroupTest.java
@@ -0,0 +1,48 @@
+package jenkins.advancedqueue.test;
+
+import hudson.model.Cause.UserIdCause;
+import jenkins.advancedqueue.testutil.ExpectedItem;
+import jenkins.advancedqueue.testutil.JobHelper;
+import jenkins.advancedqueue.testutil.TestRunListener;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.recipes.LocalData;
+
+public class JobPatternGroupTest {
+
+	@Rule
+	public JenkinsRule j = new JenkinsRule();
+
+	private JobHelper jobHelper = new JobHelper(j);
+
+	@Test
+	@LocalData
+	public void test_job_pattern_1() throws Exception {
+		TestRunListener.init(new ExpectedItem("Job 0", 3));
+		jobHelper.scheduleProjects(new UserIdCause());
+		j.waitUntilNoActivity();
+		TestRunListener.assertStartedItems();
+	}
+
+	@Test
+	@LocalData
+	public void test_job_pattern_2() throws Exception {
+		TestRunListener.init(new ExpectedItem("Job 0", 3), new ExpectedItem("Job 1", 9));
+		jobHelper.scheduleProjects(new UserIdCause(), new UserIdCause());
+		j.waitUntilNoActivity();
+		TestRunListener.assertStartedItems();
+	}
+
+	@Test
+	@LocalData
+	public void test_job_pattern_3() throws Exception {
+		TestRunListener.init(new ExpectedItem("Job 0", 3), new ExpectedItem("Job 3", 3), new ExpectedItem("Job 1", 9),
+				new ExpectedItem("Job 2", 9));
+		jobHelper.scheduleProjects(new UserIdCause(), new UserIdCause(), new UserIdCause(), new UserIdCause());
+		j.waitUntilNoActivity();
+		TestRunListener.assertStartedItems();
+	}
+
+}
