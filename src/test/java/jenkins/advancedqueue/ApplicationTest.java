6f74641c2021817b863f3254ed7e7f39f3800c2d
emsa23 <emsa@switchbeat.com>
2013-12-15 12:00:07 +0100

    [TEST] Added simple test framework
    Started with a simple framework for scheduling Jobs 
    and validation the priority and start order.

--- src/test/java/jenkins/advancedqueue/ApplicationTest.java
+++ src/test/java/jenkins/advancedqueue/ApplicationTest.java
@@ -1,12 +1,16 @@
 package jenkins.advancedqueue;
 
-import hudson.model.FreeStyleBuild;
+import hudson.model.Cause;
+import hudson.model.Cause.UserIdCause;
 import hudson.model.FreeStyleProject;
-import hudson.model.queue.QueueTaskFuture;
-import jenkins.advancedqueue.sorter.ItemInfo;
-import jenkins.advancedqueue.sorter.QueueItemCache;
 
-import org.junit.Assert;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import jenkins.advancedqueue.testutil.ExpectedItem;
+import jenkins.advancedqueue.testutil.TestRunListener;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -14,27 +18,49 @@ import org.jvnet.hudson.test.recipes.LocalData;
 
 public class ApplicationTest {
 
+	private final static Logger LOGGER = Logger.getLogger(ApplicationTest.class.getName());
+	
 	@Rule
 	public JenkinsRule j = new JenkinsRule();
 
+	private List<FreeStyleProject> createProjects(int numberOfProjects) throws Exception {
+		List<FreeStyleProject> projects = new ArrayList<FreeStyleProject>(numberOfProjects);
+		for (int i = 0; i < numberOfProjects; i++) {
+			FreeStyleProject project = j.createFreeStyleProject("Job " + i);
+			projects.add(project);
+		}
+		return projects;
+	}
+
+	private void scheduleProjects(Cause... causes) throws Exception {
+		List<FreeStyleProject> projects = createProjects(causes.length);
+		for (int i = 0; i < causes.length; i++) {
+			projects.get(i).scheduleBuild(1, causes[i]);
+		}
+	}
+
 	@Test
 	public void simple_with_no_configuration() throws Exception {
-		FreeStyleProject project = j.createFreeStyleProject("Job A");
-		QueueTaskFuture<FreeStyleBuild> buildFuture = project.scheduleBuild2(1);
-		ItemInfo item = QueueItemCache.get().getItem(buildFuture.get().getParent().getName());
-		Assert.assertEquals(3, item.getPriority());
-		buildFuture.get();
-		TestRunListener.assertJobStartOrder("Job A");
+		TestRunListener.init(new ExpectedItem("Job 0", 3));
+		scheduleProjects(new UserIdCause());
+		j.waitUntilNoActivity();
+		TestRunListener.assertStartedItems();		
+	}
+
+	@Test
+	public void simple_two_jobs_with_no_configuration() throws Exception {
+		TestRunListener.init(new ExpectedItem("Job 0", 3), new ExpectedItem("Job 1", 3));
+		scheduleProjects(new UserIdCause(), new UserIdCause());
+		j.waitUntilNoActivity();
+		TestRunListener.assertStartedItems();		
 	}
 
 	@Test
 	@LocalData
 	public void simple_with_basic_configuration() throws Exception {
-		FreeStyleProject project = j.createFreeStyleProject("Job A");
-		QueueTaskFuture<FreeStyleBuild> buildFuture = project.scheduleBuild2(1);
-		ItemInfo item = QueueItemCache.get().getItem(buildFuture.get().getParent().getName());
-		Assert.assertEquals(9, item.getPriority());
-		buildFuture.get();
-		TestRunListener.assertJobStartOrder("Job A");
+		TestRunListener.init(new ExpectedItem("Job 0", 9));
+		scheduleProjects(new UserIdCause());
+		j.waitUntilNoActivity();
+		TestRunListener.assertStartedItems();		
 	}
 }
