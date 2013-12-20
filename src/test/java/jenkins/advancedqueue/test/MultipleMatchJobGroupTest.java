b4743a6b21d00e1d23b858c3edad13c2155b8da2
emsa23 <emsa@switchbeat.com>
2013-12-20 23:30:55 +0100

    [TEST] [JENKINS-21103] Added test case
    Added testcase for JENKINS-21103: 
    "Priorities are assigned top down by first match" 
    does not work correctly

---
+++ src/test/java/jenkins/advancedqueue/test/MultipleMatchJobGroupTest.java
@@ -0,0 +1,32 @@
+package jenkins.advancedqueue.test;
+
+import hudson.cli.BuildCommand.CLICause;
+import hudson.model.Cause;
+import jenkins.advancedqueue.testutil.ExpectedItem;
+import jenkins.advancedqueue.testutil.JobHelper;
+import jenkins.advancedqueue.testutil.TestRunListener;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.recipes.LocalData;
+
+public class MultipleMatchJobGroupTest {
+
+	@Rule
+	public JenkinsRule j = new JenkinsRule();
+
+	private JobHelper jobHelper = new JobHelper(j);
+
+	@Test
+	@LocalData
+	public void multiple_job_group_matches() throws Exception {
+		// Job 2 and 3 matches View1 and All -> View1 is before All -> priorities are 1 and 2
+		// Job 0 and 1 matched only All -> priorities are 3 and 4
+		TestRunListener.init(new ExpectedItem("Job 2", 1), new ExpectedItem("Job 3", 2), new ExpectedItem("Job 0", 3),
+				new ExpectedItem("Job 1", 4));
+		jobHelper.scheduleProjects(new CLICause(), new Cause.UserIdCause(), new CLICause(), new Cause.UserIdCause());
+		j.waitUntilNoActivity();
+		TestRunListener.assertStartedItems();
+	}
+}
