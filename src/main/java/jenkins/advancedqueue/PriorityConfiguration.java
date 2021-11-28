24e8d6532e3d281ae26adcc3b85f19f64fd1b8e3
Mark Waite <mark.earl.waite@gmail.com>
2021-11-28 09:32:14 -0700

    Remove unused import

--- src/main/java/jenkins/advancedqueue/PriorityConfiguration.java
+++ src/main/java/jenkins/advancedqueue/PriorityConfiguration.java
@@ -46,7 +46,6 @@ import hudson.util.ListBoxModel;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
