27d8b59fe7a51b35ee0e39995814ab4efab2b15d
Simon Sohrt <sohrt@his.de>
2022-10-06 13:40:13 +0200

    [JENKINS-69751]: Wrote a test for AdvancedQueueSorter.sortBlockedItems(List<Queue.BlockedItem> blockedItems)

---
+++ src/test/java/jenkins/advancedqueue/test/SortBlockedItemsTest.java
@@ -0,0 +1,44 @@
+package jenkins.advancedqueue.test;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.recipes.LocalData;
+
+import hudson.model.FreeStyleProject;
+import hudson.model.Queue;
+import hudson.tasks.BuildTrigger;
+import jenkins.advancedqueue.testutil.ExpectedItem;
+import jenkins.advancedqueue.testutil.TestRunListener;
+
+public class SortBlockedItemsTest {
+
+	@Rule
+	public JenkinsRule j = new JenkinsRule();
+	
+	@Test
+	@LocalData
+	public void blockedItemsAreSortedByPriority() throws Exception {
+		//Priority value is configured in jenkins.advancedqueue.PrioritySorterConfiguration. Value is 2. 
+		FreeStyleProject upstreamProject = j.createFreeStyleProject("upstreamProject");
+		//Priority value is configured in jenkins.advancedqueue.PrioritySorterConfiguration. Value is 1. 
+		FreeStyleProject downstreamProject = j.createFreeStyleProject("downstreamProject");
+		upstreamProject.setBlockBuildWhenDownstreamBuilding(true);
+		downstreamProject.setBlockBuildWhenUpstreamBuilding(true);
+		upstreamProject.getPublishersList().add(new BuildTrigger(downstreamProject.getName(), true));
+		// Expected build sequence: downstreamProject#1 -> upstreamProject#1 -> downstreamProject#2 
+		TestRunListener.init(new ExpectedItem(downstreamProject.getName(), 1), new ExpectedItem(upstreamProject.getName(), 2), new ExpectedItem(downstreamProject.getName(), 1));
+		
+		j.jenkins.rebuildDependencyGraph();
+		
+		// Locking makes sure that the queue sees both builds at the same time even though upstreamProject entered the queue first
+		Queue.withLock(() -> {
+			j.jenkins.getQueue().schedule2(upstreamProject, 0);
+			j.jenkins.getQueue().schedule2(downstreamProject, 0);
+			j.jenkins.getQueue().maintain();
+		});
+		j.waitUntilNoActivity();
+		
+		TestRunListener.assertStartedItems();
+	}
+}
