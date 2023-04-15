9454df5e805c82bd9e32fecbd25b8051f0226fb4
Mark Waite <mark.earl.waite@gmail.com>
2023-04-14 22:23:57 -0600

    Do not lose icon impl

--- src/main/java/jenkins/advancedqueue/PriorityConfiguration.java
+++ src/main/java/jenkins/advancedqueue/PriorityConfiguration.java
@@ -59,6 +59,7 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.acegisecurity.context.SecurityContext;
 import org.acegisecurity.context.SecurityContextHolder;
+import org.jenkins.ui.icon.IconSpec;
 import org.jenkinsci.plugins.workflow.support.steps.ExecutorStepExecution;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
@@ -70,7 +71,7 @@ import org.kohsuke.stapler.StaplerResponse;
  */
 @Extension
 public class PriorityConfiguration extends Descriptor<PriorityConfiguration>
-        implements RootAction, Describable<PriorityConfiguration> {
+        implements RootAction, IconSpec, Describable<PriorityConfiguration> {
 
     private static final Logger LOGGER = Logger.getLogger(PriorityConfiguration.class.getName());
 
@@ -104,11 +105,18 @@ public class PriorityConfiguration extends Descriptor<PriorityConfiguration>
         }
     }
 
+    @Override
     public String getIconFileName() {
+        return null;
+    }
+
+    @Override
+    public String getIconClassName() {
         if (!checkActive()) {
             return null;
         }
-        return "/plugin/PrioritySorter/advqueue.svg";
+
+        return "symbol-swap-vertical-outline plugin-ionicons-api";
     }
 
     @Override
