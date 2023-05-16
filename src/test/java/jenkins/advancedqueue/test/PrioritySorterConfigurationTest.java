2553d3ea06e8b867ab21e53b14b5314851eae815
Huikun Zheng <huikunzheng9955@gmail.com>
2023-05-15 20:00:59 -0700

    tests bug fix

--- src/test/java/jenkins/advancedqueue/test/PrioritySorterConfigurationTest.java
+++ src/test/java/jenkins/advancedqueue/test/PrioritySorterConfigurationTest.java
@@ -1,24 +1,24 @@
 package jenkins.advancedqueue.test;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import hudson.util.ListBoxModel;
+import java.util.Arrays;
+import java.util.List;
 import jenkins.advancedqueue.PrioritySorterConfiguration;
 import jenkins.advancedqueue.sorter.SorterStrategy;
 import jenkins.advancedqueue.sorter.SorterStrategyDescriptor;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import hudson.util.ListBoxModel;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.mockito.MockedStatic;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
 
 public class PrioritySorterConfigurationTest {
     @Rule
     public JenkinsRule j = new JenkinsRule();
+
     private PrioritySorterConfiguration prioritySorterConfiguration;
     private SorterStrategyDescriptor sorterStrategyDescriptor1, sorterStrategyDescriptor2;
 
@@ -36,7 +36,8 @@ public class PrioritySorterConfigurationTest {
         when(sorterStrategyDescriptor2.getDisplayName()).thenReturn("DisplayName2");
         when(sorterStrategyDescriptor2.getKey()).thenReturn("Key2");
 
-        List<SorterStrategyDescriptor> mockStrategies = Arrays.asList(sorterStrategyDescriptor1, sorterStrategyDescriptor2);
+        List<SorterStrategyDescriptor> mockStrategies =
+                Arrays.asList(sorterStrategyDescriptor1, sorterStrategyDescriptor2);
 
         try (MockedStatic<SorterStrategy> mocked = mockStatic(SorterStrategy.class)) {
             mocked.when(SorterStrategy::getAllSorterStrategies).thenReturn(mockStrategies);
