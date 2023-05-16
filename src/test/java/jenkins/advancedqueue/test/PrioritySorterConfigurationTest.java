ee1d0c40ea8b9aceeec243ad6ed272b4ea276bc5
Huikun Zheng <huikunzheng9955@gmail.com>
2023-05-15 19:10:39 -0700

    tests bug fix

--- src/test/java/jenkins/advancedqueue/test/PrioritySorterConfigurationTest.java
+++ src/test/java/jenkins/advancedqueue/test/PrioritySorterConfigurationTest.java
@@ -4,8 +4,12 @@ import jenkins.advancedqueue.PrioritySorterConfiguration;
 import jenkins.advancedqueue.sorter.SorterStrategy;
 import jenkins.advancedqueue.sorter.SorterStrategyDescriptor;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import hudson.util.ListBoxModel;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.mockito.MockedStatic;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,6 +17,8 @@ import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
 public class PrioritySorterConfigurationTest {
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
     private PrioritySorterConfiguration prioritySorterConfiguration;
     private SorterStrategyDescriptor sorterStrategyDescriptor1, sorterStrategyDescriptor2;
 
@@ -31,14 +37,17 @@ public class PrioritySorterConfigurationTest {
         when(sorterStrategyDescriptor2.getKey()).thenReturn("Key2");
 
         List<SorterStrategyDescriptor> mockStrategies = Arrays.asList(sorterStrategyDescriptor1, sorterStrategyDescriptor2);
-        when(SorterStrategy.getAllSorterStrategies()).thenReturn(mockStrategies);
 
-        ListBoxModel result = prioritySorterConfiguration.doFillStrategyItems();
+        try (MockedStatic<SorterStrategy> mocked = mockStatic(SorterStrategy.class)) {
+            mocked.when(SorterStrategy::getAllSorterStrategies).thenReturn(mockStrategies);
 
-        assertEquals(2, result.size());
-        assertEquals("DisplayName1", result.get(0).name);
-        assertEquals("Key1", result.get(0).value);
-        assertEquals("DisplayName2", result.get(1).name);
-        assertEquals("Key2", result.get(1).value);
+            ListBoxModel result = prioritySorterConfiguration.doFillStrategyItems();
+
+            assertEquals(2, result.size());
+            assertEquals("DisplayName1", result.get(0).name);
+            assertEquals("Key1", result.get(0).value);
+            assertEquals("DisplayName2", result.get(1).name);
+            assertEquals("Key2", result.get(1).value);
+        }
     }
 }
