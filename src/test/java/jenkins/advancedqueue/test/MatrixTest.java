82d5abac8b27f09e906f8572e1a14519a4bcb077
Huikun Zheng <huikunzheng9955@gmail.com>
2023-05-15 20:13:09 -0700

    tests bug fix

--- src/test/java/jenkins/advancedqueue/test/MatrixTest.java
+++ src/test/java/jenkins/advancedqueue/test/MatrixTest.java
@@ -17,63 +17,65 @@ public class MatrixTest {
 
     private JobHelper jobHelper = new JobHelper(j);
 
-        @Test
-        @LocalData
-        public void simple_matrix_with_no_configuration() throws Exception {
-            TestRunListener.init(
-                    new ExpectedItem("Matrix 0", 1), new ExpectedItem("0A1=0A.", 1), new ExpectedItem("0A1=0A.", 1));
-            jobHelper.scheduleMatrixProjects(new Cause.UserIdCause()).go();
-            j.waitUntilNoActivity();
-            TestRunListener.assertStartedItems();
-        }
+    @Test
+    @LocalData
+    public void simple_matrix_with_no_configuration() throws Exception {
+        TestRunListener.init(
+                new ExpectedItem("Matrix 0", 1), new ExpectedItem("0A1=0A.", 1), new ExpectedItem("0A1=0A.", 1));
+        jobHelper.scheduleMatrixProjects(new Cause.UserIdCause()).go();
+        j.waitUntilNoActivity();
+        TestRunListener.assertStartedItems();
+    }
 
-        @Test
-        @LocalData
-        public void simple_two_matrix_with_no_configuration() throws Exception {
-            TestRunListener.init(
-                    new ExpectedItem("Matrix 0", 1), new ExpectedItem("Matrix 1", 1),
-                    new ExpectedItem("0A1=0A.", 1), new ExpectedItem("0A1=0A.", 1),
-                    new ExpectedItem("1A1=1A.", 1), new ExpectedItem("1A1=1A.", 1));
-            jobHelper.scheduleMatrixProjects(new Cause.UserIdCause(), new Cause.UserIdCause()).go();
-            j.waitUntilNoActivity();
-            TestRunListener.assertStartedItems();
-        }
+    @Test
+    @LocalData
+    public void simple_two_matrix_with_no_configuration() throws Exception {
+        TestRunListener.init(
+                new ExpectedItem("Matrix 0", 1), new ExpectedItem("Matrix 1", 1),
+                new ExpectedItem("0A1=0A.", 1), new ExpectedItem("0A1=0A.", 1),
+                new ExpectedItem("1A1=1A.", 1), new ExpectedItem("1A1=1A.", 1));
+        jobHelper
+                .scheduleMatrixProjects(new Cause.UserIdCause(), new Cause.UserIdCause())
+                .go();
+        j.waitUntilNoActivity();
+        TestRunListener.assertStartedItems();
+    }
 
-        @Test
-        @LocalData
-        public void matrix_and_jobs_with_no_configuration() throws Exception {
-            TestRunListener.init(
-                    new ExpectedItem("Matrix 0", 1),
-                    new ExpectedItem("Matrix 1", 5),
-                    new ExpectedItem("0A1=0A.", 1),
-                    new ExpectedItem("0A1=0A.", 1),
-                    new ExpectedItem("Job 0", 5),
-                    new ExpectedItem("1A1=1A.", 5),
-                    new ExpectedItem("1A1=1A.", 5));
-            jobHelper
-                    .scheduleProjects(new BuildCommand.CLICause())
-                    .scheduleMatrixProjects(new Cause.UserIdCause(), new BuildCommand.CLICause())
-                    .go();
-            j.waitUntilNoActivity();
-            TestRunListener.assertStartedItems();
-        }
+    @Test
+    @LocalData
+    public void matrix_and_jobs_with_no_configuration() throws Exception {
+        TestRunListener.init(
+                new ExpectedItem("Matrix 0", 1),
+                new ExpectedItem("Matrix 1", 5),
+                new ExpectedItem("0A1=0A.", 1),
+                new ExpectedItem("0A1=0A.", 1),
+                new ExpectedItem("Job 0", 5),
+                new ExpectedItem("1A1=1A.", 5),
+                new ExpectedItem("1A1=1A.", 5));
+        jobHelper
+                .scheduleProjects(new BuildCommand.CLICause())
+                .scheduleMatrixProjects(new Cause.UserIdCause(), new BuildCommand.CLICause())
+                .go();
+        j.waitUntilNoActivity();
+        TestRunListener.assertStartedItems();
+    }
 
-        @Test
-        @LocalData
-        public void matrix_and_jobs_with_no_configuration_reverse() throws Exception {
-            TestRunListener.init(
-                    new ExpectedItem("Matrix 0", 1),
-                    new ExpectedItem("Matrix 1", 5),
-                    new ExpectedItem("0A1=0A.", 1),
-                    new ExpectedItem("0A1=0A.", 1),
-                    new ExpectedItem("1A1=1A.", 5),
-                    new ExpectedItem("1A1=1A.", 5),
-                    new ExpectedItem("Job 0", 5));
-            jobHelper
-                    .scheduleMatrixProjects(new Cause.UserIdCause(), new BuildCommand.CLICause())
-                    .scheduleProjects(new BuildCommand.CLICause())
-                    .go();
-            j.waitUntilNoActivity();
-            TestRunListener.assertStartedItems();
-        }
+    @Test
+    @LocalData
+    public void matrix_and_jobs_with_no_configuration_reverse() throws Exception {
+        TestRunListener.init(
+                new ExpectedItem("Matrix 0", 1),
+                new ExpectedItem("Matrix 1", 5),
+                new ExpectedItem("0A1=0A.", 1),
+                new ExpectedItem("0A1=0A.", 1),
+                new ExpectedItem("1A1=1A.", 5),
+                new ExpectedItem("1A1=1A.", 5),
+                new ExpectedItem("Job 0", 5));
+        jobHelper
+                .scheduleMatrixProjects(new Cause.UserIdCause(), new BuildCommand.CLICause())
+                .scheduleProjects(new BuildCommand.CLICause())
+                .go();
+        j.waitUntilNoActivity();
+        TestRunListener.assertStartedItems();
+    }
 }
