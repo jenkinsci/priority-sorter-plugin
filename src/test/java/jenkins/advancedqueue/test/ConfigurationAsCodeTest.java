a5103922f7b679bd86e638261f7daed098386603
Mark Waite <mark.earl.waite@gmail.com>
2023-06-09 20:34:45 -0600

    Use hamcrest assertThat, not deprecated JUnit assertThat

--- src/test/java/jenkins/advancedqueue/test/ConfigurationAsCodeTest.java
+++ src/test/java/jenkins/advancedqueue/test/ConfigurationAsCodeTest.java
@@ -1,7 +1,7 @@
 package jenkins.advancedqueue.test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
 
 import io.jenkins.plugins.casc.ConfigurationAsCode;
 import io.jenkins.plugins.casc.ConfiguratorException;
