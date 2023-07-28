240b2b8c31d3dad55e3581bb8a76c909a0001eb4
Mark Waite <mark.earl.waite@gmail.com>
2023-07-28 16:13:57 -0600

    Exclude another unreliable windows test

--- src/test/java/jenkins/advancedqueue/test/MatrixTest.java
+++ src/test/java/jenkins/advancedqueue/test/MatrixTest.java
@@ -32,6 +32,7 @@ public class MatrixTest {
     @Test
     @LocalData
     public void simple_two_matrix_with_no_configuration() throws Exception {
+        assumeTrue("Test unreliable on Windows", !isWindows());
         TestRunListener.init(
                 new ExpectedItem("Matrix 0", 1), new ExpectedItem("Matrix 1", 1),
                 new ExpectedItem("0A1=0A.", 1), new ExpectedItem("0A1=0A.", 1),
