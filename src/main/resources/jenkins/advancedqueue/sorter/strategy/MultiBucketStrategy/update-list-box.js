f85e62da9a2b8aabdc0f438a10d74046b630eb59
Yaroslav Afenkin <91559310+yaroslavafenkin@users.noreply.github.com>
2024-10-21 14:35:23 +0300

    [JENKINS-73910] Extract event handler in MultiBucketStrategy/config.jelly

---
+++ src/main/resources/jenkins/advancedqueue/sorter/strategy/MultiBucketStrategy/update-list-box.js
@@ -0,0 +1,10 @@
+Behaviour.specify(".ps_numberOfPriorities", "MultiBucketStrategy_updateListBox", 0, function(element) {
+    element.addEventListener("change", function(event) {
+        const target = event.target;
+        const descriptorKey = target.dataset.descriptorKey;
+        const descriptorUrl = target.dataset.descriptorUrl;
+        const defaultPriority = document.querySelector(`#ps_defaultPriority-${descriptorKey}`);
+
+        updateListBox(defaultPriority, `${descriptorUrl}/updateDefaultPriorityItems?value=${target.value}`);
+    });
+});
