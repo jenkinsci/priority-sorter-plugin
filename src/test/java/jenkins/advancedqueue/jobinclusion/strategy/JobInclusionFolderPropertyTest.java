f0c04a68384cf9854f296ca077e765a84c2a90d5
Mark Waite <mark.earl.waite@gmail.com>
2024-12-29 06:03:04 -0700

    Remove queue related assertions

--- src/test/java/jenkins/advancedqueue/jobinclusion/strategy/JobInclusionFolderPropertyTest.java
+++ src/test/java/jenkins/advancedqueue/jobinclusion/strategy/JobInclusionFolderPropertyTest.java
@@ -5,11 +5,9 @@ import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import hudson.model.FreeStyleProject;
-import hudson.model.Queue;
 import hudson.model.Run;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -30,12 +28,6 @@ public class JobInclusionFolderPropertyTest {
         Run r = project.scheduleBuild2(0).get(); // Schedule a build to ensure the queue item is created
         j.assertBuildStatusSuccess(r);
         property = new JobInclusionFolderProperty(true, "testGroup");
-
-        // Ensure the custom QueueSorter is used
-        project.getDescriptor();
-
-        Queue.Item queueItem = project.getQueueItem();
-        assertNull("Queue.Item should be null", queueItem);
     }
 
     @Test
