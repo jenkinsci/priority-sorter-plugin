232a230c87788190198b0a94427c0511b45275cb
Mark Waite <mark.earl.waite@gmail.com>
2025-01-04 09:14:59 -0700

    Test more of the private implementation
    Allocate a strategy and test its descriptor

--- src/test/java/jenkins/advancedqueue/sorter/SorterStrategyDescriptorTest.java
+++ src/test/java/jenkins/advancedqueue/sorter/SorterStrategyDescriptorTest.java
@@ -20,16 +20,30 @@ public class SorterStrategyDescriptorTest {
 
     private static FreeStyleProject project;
     private static FreeStyleBuild build;
+    private static SorterStrategy strategy;
     private static SorterStrategyDescriptor descriptor;
 
     private static final String STRATEGY_NAME = "strategy short name";
+    private static final int NUMBER_OF_PRIORITIES = 9;
+    private static final int DEFAULT_PRIORITY = 4;
 
     @BeforeClass
     public static void runJob() throws Exception {
         project = j.createFreeStyleProject();
         build = project.scheduleBuild2(0).get();
-        descriptor = new TestSorterStrategy().getDescriptor();
+        strategy = new TestSorterStrategy();
+        descriptor = strategy.getDescriptor();
         j.assertBuildStatusSuccess(build);
+    }
+
+    @Test
+    public void getNumberOfPriorities() {
+        assertEquals(NUMBER_OF_PRIORITIES, strategy.getNumberOfPriorities());
+    }
+
+    @Test
+    public void getDefaultPriority() {
+        assertEquals(DEFAULT_PRIORITY, strategy.getDefaultPriority());
     }
 
     @Test
@@ -55,22 +69,20 @@ public class SorterStrategyDescriptorTest {
 
         @Override
         public int getNumberOfPriorities() {
-            return 1;
+            return NUMBER_OF_PRIORITIES;
         }
 
         @Override
         public int getDefaultPriority() {
-            return 1;
+            return DEFAULT_PRIORITY;
         }
 
         @Extension
-        public static class DescriptorImpl extends TestSorterStrategyDescriptor {}
-    }
-
-    private static class TestSorterStrategyDescriptor extends SorterStrategyDescriptor {
-        @Override
-        public String getShortName() {
-            return STRATEGY_NAME;
+        public static class DescriptorImpl extends SorterStrategyDescriptor {
+            @Override
+            public String getShortName() {
+                return STRATEGY_NAME;
+            }
         }
     }
 }
