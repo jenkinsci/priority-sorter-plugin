9b9cc1e297253641de676c23622bb25583550533
Yash Pal <yashpal2104@gmail.com>
2025-01-05 22:52:53 +0530

    Fixed typo

--- src/main/java/jenkins/advancedqueue/sorter/ItemInfo.java
+++ src/main/java/jenkins/advancedqueue/sorter/ItemInfo.java
@@ -196,7 +196,7 @@ public class ItemInfo
                 itemId, jobName, jobGroupId, reason, priority, weight, itemStatus);
     }
 
-    public String getDescisionLog() {
+    public String getDecisionLog() {
         StringBuilder buffer = new StringBuilder();
         for (String log : decisionLog) {
             buffer.append(log).append("\n");
